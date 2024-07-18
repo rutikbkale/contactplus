bcb91b6af927efbc5aacafc1ae08ac159d311af8
kalerutik <kalerutik656@gmail.com>
2024-07-18 10:31:02 +0530

    creating signup & signin ui

--- src/main/java/com/contactplus/controllers/MainController.java
+++ src/main/java/com/contactplus/controllers/MainController.java
@@ -3,12 +3,24 @@ package com.contactplus.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-public class MyController {
+public class MainController {
 
     @RequestMapping("/")
     public String landingPage(){
         return "index";
     }
+
+    @RequestMapping("/signup")
+    public String signupPage() {
+        return new String("signup");
+    }
+
+    @RequestMapping("/signin")
+    public String signinPage() {
+        return new String("signin");
+    }
+    
 
 }
