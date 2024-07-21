45943ed94d3333a2dfe8f2210f5a3a03efad2819
kalerutik <kalerutik656@gmail.com>
2024-07-21 19:47:23 +0530

    creating user dashboard

--- src/main/java/com/contactplus/controllers/UserController.java
+++ src/main/java/com/contactplus/controllers/UserController.java
@@ -4,8 +4,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.contactplus.helpers.Helper;
-
 @Controller
 @RequestMapping("/contactplus/user")
 public class UserController {
